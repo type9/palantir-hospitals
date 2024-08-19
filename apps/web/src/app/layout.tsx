0cff521fb6a4ca5ca2e07221a2d04d3e05144738
type9 <gabespersonal@gmail.com>
2024-08-19 16:12:13 +0800

    refactor: removes old code from forked repo

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,14 +1,7 @@
 import "./globals.css"
 
-import { Suspense } from "react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import classNames from "classnames"
 
-import { AnalyticsWrapper } from "@/components/Analytics/AnalyticsWrapper"
-import { GlobalLayout } from "@/components/Layouts/GlobalLayout"
-import { Loading } from "@/components/Loading"
-import { ExternalInputProvider } from "@/components/MidiInput/ExternalInputProvider"
-import { UserProvider } from "@/components/Providers/UserProvider"
 import { fontIndex } from "@/fonts/fontIndex"
 import { DEFAULT_METADATA } from "@/webmeta/metadata/defaultMetadata"
 
@@ -36,20 +29,6 @@ export default function RootLayout({
 			</head>
 			<body>
 				<div id="portal-root" className="transition"></div>
-				<UserProvider>
-					<ExternalInputProvider>
-						<GlobalLayout
-							title={"ColorChords"}
-							displaySearch={true}
-						>
-							<Suspense fallback={<Loading />}>
-								{children}
-							</Suspense>
-							<AnalyticsWrapper />
-							<SpeedInsights />
-						</GlobalLayout>
-					</ExternalInputProvider>
-				</UserProvider>
 			</body>
 		</html>
 	)
--- apps/web/src/components/Breadcrumbs/index.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import styles from "./Breadcrumbs.module.css"
-
-export default function Breadcrumbs({}) {
-	return <div className={styles.main}></div>
-}
