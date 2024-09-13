83b7bc9a9ff61c1c781195aaf9096ecc0e660420
type9 <gabespersonal@gmail.com>
2024-09-13 17:17:54 +0800

    refactor: changes to newyork

--- apps/web/src/app/(app)/dashboard/page.tsx
+++ apps/web/src/app/(app)/dashboard/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from "next"
 import Image from "next/image"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@radix-ui/react-tabs"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -10,12 +9,10 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { MainNav } from "./components/main-nav"
 import { Overview } from "./components/overview"
 import { RecentSales } from "./components/recent-sales"
-import { Search } from "./components/search"
-import TeamSwitcher from "./components/team-switcher"
-import { UserNav } from "./components/user-nav"
 
 export const metadata: Metadata = {
 	title: "Dashboard",
@@ -25,16 +22,26 @@ export const metadata: Metadata = {
 export default function DashboardPage() {
 	return (
 		<>
-			<div className="md:hidden"></div>
+			<div className="md:hidden">
+				<Image
+					src="/examples/dashboard-light.png"
+					width={1280}
+					height={866}
+					alt="Dashboard"
+					className="block dark:hidden"
+				/>
+				<Image
+					src="/examples/dashboard-dark.png"
+					width={1280}
+					height={866}
+					alt="Dashboard"
+					className="hidden dark:block"
+				/>
+			</div>
 			<div className="hidden flex-col md:flex">
 				<div className="border-b">
 					<div className="flex h-16 items-center px-4">
-						<TeamSwitcher />
 						<MainNav className="mx-6" />
-						<div className="ml-auto flex items-center space-x-4">
-							<Search />
-							<UserNav />
-						</div>
 					</div>
 				</div>
 				<div className="flex-1 space-y-4 p-8 pt-6">
