f2c354a885ad62af2fcb4363cb29235dfc266178
type9 <gabespersonal@gmail.com>
2024-10-05 10:24:20 +0800

    fix: fixes bad responsive layout

--- apps/web/src/app/(app)/dashboard/page.tsx
+++ apps/web/src/app/(app)/dashboard/page.tsx
@@ -20,52 +20,31 @@ import { tabContent } from "./content"
 
 export default function DashboardPage() {
 	return (
-		<>
-			<div className="md:hidden">
-				<Image
-					src="/examples/dashboard-light.png"
-					width={1280}
-					height={866}
-					alt="Dashboard"
-					className="block dark:hidden"
-				/>
-				<Image
-					src="/examples/dashboard-dark.png"
-					width={1280}
-					height={866}
-					alt="Dashboard"
-					className="hidden dark:block"
-				/>
+		<div className="flex-1 space-y-4 p-8 pt-6">
+			<div className="flex items-center justify-between space-y-2">
+				<h2 className="text-3xl font-bold tracking-tight">
+					COVID-19 Crisis Tracker
+				</h2>
+				<div className="flex items-center space-x-2"></div>
 			</div>
-			<div className="hidden flex-col md:flex">
-				<div className="border-b"></div>
-				<div className="flex-1 space-y-4 p-8 pt-6">
-					<div className="flex items-center justify-between space-y-2">
-						<h2 className="text-3xl font-bold tracking-tight">
-							COVID-19 Crisis Tracker
-						</h2>
-						<div className="flex items-center space-x-2"></div>
-					</div>
-					<Tabs defaultValue={"overview"} className="space-y-4">
-						<TabsList>
-							{tabContent.map((tab) => (
-								<TabsTrigger key={tab.id} value={tab.id}>
-									{tab.title}
-								</TabsTrigger>
-							))}
-						</TabsList>
-						{tabContent.map((tab) => (
-							<TabsContent
-								className="space-y-4"
-								key={tab.id}
-								value={tab.id}
-							>
-								{tab.content && <tab.content />}
-							</TabsContent>
-						))}
-					</Tabs>
-				</div>
-			</div>
-		</>
+			<Tabs defaultValue={"overview"} className="space-y-4">
+				<TabsList>
+					{tabContent.map((tab) => (
+						<TabsTrigger key={tab.id} value={tab.id}>
+							{tab.title}
+						</TabsTrigger>
+					))}
+				</TabsList>
+				{tabContent.map((tab) => (
+					<TabsContent
+						className="space-y-4"
+						key={tab.id}
+						value={tab.id}
+					>
+						{tab.content && <tab.content />}
+					</TabsContent>
+				))}
+			</Tabs>
+		</div>
 	)
 }
